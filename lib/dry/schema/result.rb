4f74134cc6090e96ee092e44d3f6116c55fb49bc
Piotr Solnica <piotr.solnica+oss@gmail.com>
2018-02-18 15:51:30 +0100

    Restore Result#messages

--- lib/dry/schema/result.rb
+++ lib/dry/schema/result.rb
@@ -26,6 +26,10 @@ module Dry
         message_set(options.merge(hints: false)).dump
       end
 
+      def messages(options = EMPTY_HASH)
+        message_set(options.merge(hints: true)).dump
+      end
+
       def message_set(options = EMPTY_HASH)
         message_compiler.with(options).(result_ast)
       end
