0528f7bff159f42767ffecb78192c156a2a77781
Piotr Solnica <piotr.solnica@gmail.com>
2018-10-02 11:48:46 +0200

    Port schema unit specs

--- spec/unit/dry/schema/schema_spec.rb
+++ spec/unit/dry/schema/schema_spec.rb
@@ -4,31 +4,33 @@ RSpec.describe Dry::Schema do
   describe '.messages' do
     context 'with default setting' do
       let(:schema) do
-        Class.new(Dry::Schema)
+        Dry::Schema.define {}
       end
 
       it 'returns default yaml messages' do
-        expect(schema.messages).to be_instance_of(Dry::Schema::Messages::YAML)
+        expect(schema.message_compiler.messages)
+          .to be_instance_of(Dry::Schema::Messages::YAML)
       end
     end
 
     context 'with i18n setting' do
       let(:schema) do
-        Class.new(Dry::Schema) { configure { config.messages = :i18n } }
+        Dry::Schema.define { configure { config.messages = :i18n } }
       end
 
       it 'returns default i18n messages' do
-        expect(schema.messages).to be_instance_of(Dry::Schema::Messages::I18n)
+        expect(schema.message_compiler.messages)
+          .to be_instance_of(Dry::Schema::Messages::I18n)
       end
     end
 
     context 'with an invalid setting' do
       let(:schema) do
-        Class.new(Dry::Schema) { configure { config.messages = :oops } }
+        Dry::Schema.define { configure { config.messages = :oops } }
       end
 
       it 'returns default i18n messages' do
-        expect { schema.messages }.to raise_error(RuntimeError, /oops/)
+        expect { schema }.to raise_error(RuntimeError, /oops/)
       end
     end
   end
