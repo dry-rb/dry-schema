0ca960b1850e4fafae408002b4cf0a7ebb2e1f2f
Piotr Solnica <piotr.solnica@gmail.com>
2019-01-23 13:33:50 +0100

    Simplify Definition#call a bit

--- lib/dry/schema/definition.rb
+++ lib/dry/schema/definition.rb
@@ -1,5 +1,6 @@
 require 'dry/initializer'
 
+require 'dry/schema/constants'
 require 'dry/schema/config'
 require 'dry/schema/result'
 require 'dry/schema/messages'
@@ -17,13 +18,13 @@ module Dry
       option :message_compiler, default: proc { MessageCompiler.new(Messages.setup(config)) }
 
       def call(input)
-        results = rules.reduce([]) { |a, (name, rule)|
-          unless input.error?(name)
-            result = rule.(input)
-            a << result unless result.success?
-          end
-          a
-        } || EMPTY_ARRAY
+        results = EMPTY_ARRAY.dup
+
+        rules.each do |name, rule|
+          next if input.error?(name)
+          result = rule.(input)
+          results << result if result.failure?
+        end
 
         input.concat(results)
       end
