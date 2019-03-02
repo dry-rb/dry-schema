0a71fea222ffe6eecac2cfc6d6b8da88910f80cb
Nikita Shilnikov <fg@flashgordon.ru>
2019-03-02 15:52:57 +0300

    Revert "Revert "Merge pull request #68 from dry-rb/use-updated-schemas""
    This reverts commit f24c0b0cf85b081ba840264a2df0e2a235a8344c.

--- lib/dry/schema/macros/hash.rb
+++ lib/dry/schema/macros/hash.rb
@@ -12,7 +12,34 @@ module Dry
         # @api private
         def call(*args, &block)
           trace << hash?
-          super(*args, &block)
+
+          super(*args) unless args.empty?
+
+          if block
+            definition = schema_dsl.new(&block)
+
+            parent_type = schema_dsl.types[name]
+            definition_schema = definition.type_schema
+
+            schema_type =
+              if parent_type.respond_to?(:of)
+                parent_type.of(definition_schema)
+              else
+                definition_schema
+              end
+
+            final_type =
+              if parent_type.optional?
+                schema_type.optional
+              else
+                schema_type
+              end
+
+            schema_dsl.set_type(name, final_type)
+
+            trace << definition.to_rule
+          end
+
           self
         end
       end
