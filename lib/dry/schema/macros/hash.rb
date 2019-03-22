88a70e9fbae2183001e87be9d35110045ff181ba
Nikita Shilnikov <fg@flashgordon.ru>
2019-03-22 18:20:26 +0300

    Remove duplicated branch
    :100: this is a leftover after confict resolution

--- lib/dry/schema/macros/hash.rb
+++ lib/dry/schema/macros/hash.rb
@@ -12,35 +12,7 @@ module Dry
         # @api private
         def call(*args, &block)
           trace << hash?
-
-          super(*args) unless args.empty?
-
-          if block
-            definition = schema_dsl.new(&block)
-
-            parent_type = schema_dsl.types[name]
-            definition_schema = definition.type_schema
-
-            schema_type =
-              if parent_type.respond_to?(:of)
-                parent_type.of(definition_schema)
-              else
-                definition_schema
-              end
-
-            final_type =
-              if parent_type.optional?
-                schema_type.optional
-              else
-                schema_type
-              end
-
-            schema_dsl.set_type(name, final_type)
-
-            trace << definition.to_rule
-          end
-
-          self
+          super(*args, &block)
         end
       end
     end
