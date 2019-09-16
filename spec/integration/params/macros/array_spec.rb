d58db877f05cfd85c30c9682e5a6c47c8c2bb912
Ian White <ian.w.white@gmail.com>
2019-09-16 09:27:32 +0100

    Adds support for multiple schema parents [#42]

--- spec/integration/params/macros/array_spec.rb
+++ spec/integration/params/macros/array_spec.rb
@@ -35,5 +35,27 @@ RSpec.describe 'Params / Macros / array' do
       expect(result.errors.to_h).to eql(user: ['must be an array'])
       expect(result.to_h).to eql(id: 1, user: nil)
     end
+
+    context 'from multiple parents' do
+      subject(:schema) do
+        Dry::Schema.Params(parent: [parent, parent2]) do
+          optional(:user).array(:hash)
+        end
+      end
+
+      let(:parent2) do
+        Dry::Schema.Params do
+          optional(:age).value(:integer, gt?: 17)
+        end
+      end
+
+      it 'applies coercion and rules from both parents' do
+        result = schema.('id' => '1', 'age' => '12', 'user' => nil)
+
+        expect(result.errors.to_h).to eql(user: ['must be an array'],
+                                          age: ['must be greater than 17'])
+        expect(result.to_h).to eql(id: 1, user: nil, age: 12)
+      end
+    end
   end
 end
