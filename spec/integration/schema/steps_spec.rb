71199deafbb8d1eded1ad54ad2f43a6b06781d26
Piotr Solnica <piotr.solnica@gmail.com>
2020-03-06 10:38:45 +0100

    Add a pending spec for importing callbacks from array with hashes

--- spec/integration/schema/steps_spec.rb
+++ spec/integration/schema/steps_spec.rb
@@ -50,6 +50,38 @@ RSpec.describe Dry::Schema, 'callbacks' do
     end
   end
 
+  context 'under a nested schema in an array' do
+    subject(:schema) do
+      Dry::Schema.define do
+        required(:accounts).array(:hash) do
+          required(:user).hash(Test::UserSchema)
+        end
+      end
+    end
+
+    before do
+      Test::UserSchema = Dry::Schema.define do
+        optional(:name).maybe(:string)
+        required(:date).maybe(:date)
+
+        before(:key_coercer) do |result|
+          { name: 'default' }.merge(result.to_h)
+        end
+
+        after(:rule_applier) do |result|
+          result.to_h.compact
+        end
+      end
+    end
+
+    it 'calls callbacks' do
+      pending 'not implemented yet'
+
+      expect(schema.(accounts: [{ user: { date: nil } }]).to_h)
+        .to eql(accounts: [{ user: { name: 'default' } }])
+    end
+  end
+
   context 'invalid step name' do
     it 'raises error' do
       expect {
