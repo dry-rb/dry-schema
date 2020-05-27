e967d6b61fd66f263c0840039bfec97314d20c3d
Tadeusz Niemiec <tadeusz@applover.pl>
2020-05-27 15:23:12 +0200

    allow to customize whitespace character between words per locale

--- spec/integration/localized_error_messages_spec.rb
+++ spec/integration/localized_error_messages_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Dry::Schema, "with localized messages" do
       subject(:schema) do
         Dry::Schema.define do
           config.messages.backend = :i18n
-          config.messages.load_paths = %w[en pl]
+          config.messages.load_paths = %w[en pl ja]
             .map { |l| SPEC_ROOT.join("fixtures/locales/#{l}.yml") }
 
           required(:email).value(:filled?)
@@ -21,6 +21,10 @@ RSpec.describe Dry::Schema, "with localized messages" do
             email: ["Proszę podać adres email"]
           )
         end
+
+        it "returns localized error message with defined whitespace character when full option is set to true" do
+            expect(schema.(email: "").messages(locale: :ja, full: true)).to eql(email: ["Eメールは必須入力です"])
+        end
       end
     end
 
@@ -29,7 +33,7 @@ RSpec.describe Dry::Schema, "with localized messages" do
         Dry::Schema.define do
           config.messages.backend = :i18n
           config.messages.namespace = :user
-          config.messages.load_paths = %w[en pl]
+          config.messages.load_paths = %w[en pl ja]
             .map { |l| SPEC_ROOT.join("fixtures/locales/#{l}.yml") }
 
           required(:email).value(:filled?)
@@ -51,7 +55,7 @@ RSpec.describe Dry::Schema, "with localized messages" do
           config.messages.backend = :i18n
           config.messages.namespace = :user
           config.messages.default_locale = :pl
-          config.messages.load_paths = %w[en pl]
+          config.messages.load_paths = %w[en pl ja]
             .map { |l| SPEC_ROOT.join("fixtures/locales/#{l}.yml") }
 
           required(:email).value(:filled?)
@@ -63,6 +67,20 @@ RSpec.describe Dry::Schema, "with localized messages" do
           expect(schema.(email: "").errors(locale: :en)).to eql(email: ["Please provide your email"])
 
           expect(schema.(email: "").errors).to eql(email: ["Hej user! Dawaj ten email no!"])
+        end
+
+        it "returns localized error messages" do
+          schema = Dry::Schema.define do
+            config.messages.backend = :i18n
+            config.messages.namespace = :user
+            config.messages.default_locale = :ja
+            config.messages.load_paths = %w[en pl ja]
+                                             .map { |l| SPEC_ROOT.join("fixtures/locales/#{l}.yml") }
+
+            required(:email).value(:filled?)
+          end
+
+          expect(schema.(email: "").messages(full: true)).to eql(email: ["Eメールは必須入力です"])
         end
       end
     end
