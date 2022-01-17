5a0874955222d052b1e03159d87b8d9d37af7925
Nikita Shilnikov <fg@flashgordon.ru>
2022-01-17 07:58:09 +0300

    [refactor] simplify key_validator a bit

--- lib/dry/schema/key_validator.rb
+++ lib/dry/schema/key_validator.rb
@@ -17,7 +17,6 @@ module Dry
       option :key_map
 
       # @api private
-      # rubocop: disable Metrics/PerceivedComplexity
       def call(result)
         input = result.to_h
 
@@ -25,17 +24,7 @@ module Dry
         key_paths = key_map.to_dot_notation
 
         input_paths.each do |path|
-          error_path =
-            if path[INDEX_REGEX]
-              key = path.gsub(INDEX_REGEX, BRACKETS)
-
-              if key_paths.none? { |key_path| key_path.include?(key) }
-                arr = path.gsub(INDEX_REGEX) { |m| ".#{m[1]}" }
-                arr.split(DOT).map { |s| DIGIT_REGEX.match?(s) ? s.to_i : s.to_sym }
-              end
-            elsif key_paths.none? { |key_path| key_path.include?(path) }
-              path
-            end
+          error_path = validate_path(key_paths, path)
 
           next unless error_path
 
@@ -44,35 +33,55 @@ module Dry
 
         result
       end
-      # rubocop: enable Metrics/PerceivedComplexity
 
       private
 
       # @api private
-      # rubocop: disable Metrics/AbcSize
-      # rubocop: disable Metrics/PerceivedComplexity
+      def validate_path(key_paths, path)
+        if path[INDEX_REGEX]
+          key = path.gsub(INDEX_REGEX, BRACKETS)
+
+          if key_paths.none? { _1.include?(key) }
+            arr = path.gsub(INDEX_REGEX) { ".#{_1[1]}" }
+            arr.split(DOT).map { DIGIT_REGEX.match?(_1) ? Integer(_1, 10) : _1.to_sym }
+          end
+        elsif key_paths.none? { _1.include?(path) }
+          path
+        end
+      end
+
+      # @api private
       def key_paths(hash)
-        hash.flat_map { |key, _|
-          case (value = hash[key])
-          when Hash
-            next key.to_s if value.empty?
+        hash.flat_map { |key, value|
+          case value
+          when ::Hash
+            if value.empty?
+              [key.to_s]
+            else
+              [key].product(key_paths(hash[key])).map { _1.join(DOT) }
+            end
+          when ::Array
+            hashes_or_arrays = hashes_or_arrays(value)
 
-            [key].product(key_paths(hash[key])).map { |keys| keys.join(DOT) }
-          when Array
-            hashes_or_arrays = value.select { |e| (e.is_a?(Array) || e.is_a?(Hash)) && !e.empty? }
-
-            next key.to_s if hashes_or_arrays.empty?
-
-            hashes_or_arrays.flat_map.with_index { |el, idx|
-              key_paths(el).map { |path| ["#{key}[#{idx}]", *path].join(DOT) }
-            }
+            if hashes_or_arrays.empty?
+              [key.to_s]
+            else
+              hashes_or_arrays.flat_map.with_index { |el, idx|
+                key_paths(el).map { ["#{key}[#{idx}]", *_1].join(DOT) }
+              }
+            end
           else
             key.to_s
           end
         }
       end
-      # rubocop: enable Metrics/PerceivedComplexity
-      # rubocop: enable Metrics/AbcSize
+
+      # @api private
+      def hashes_or_arrays(xs)
+        xs.select { |x|
+          (x.is_a?(::Array) || x.is_a?(::Hash)) && !x.empty?
+        }
+      end
     end
   end
 end
