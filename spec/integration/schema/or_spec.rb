13d13c4708db555511d9a7e2f10058d2102bb393
Jan Zaydowicz <jan.zaydowicz@googlemail.com>
2022-03-21 22:40:56 +0100

    add spec from PR

--- spec/integration/schema/or_spec.rb
+++ spec/integration/schema/or_spec.rb
@@ -18,6 +18,24 @@ RSpec.describe Dry::Schema, "OR messages" do
     end
   end
 
+  context "with three predicates" do
+    subject(:schema) do
+      Dry::Schema.define do
+        required(:foo) { str? | int? | bool? }
+      end
+    end
+
+    it "returns success for valid input" do
+      expect(schema.(foo: "bar")).to be_success
+      expect(schema.(foo: 321)).to be_success
+      expect(schema.(foo: true)).to be_success
+    end
+
+    it "provides OR error message for invalid input where all sides failed" do
+      expect(schema.(foo: []).errors.to_h).to eql(foo: ["must be a string or must be an integer or must be boolean"])
+    end
+  end
+
   context "with a predicate and a conjunction of predicates" do
     subject(:schema) do
       Dry::Schema.define do
@@ -109,6 +127,154 @@ RSpec.describe Dry::Schema, "OR messages" do
 
     it "provides error messages for invalid input where both sides failed" do
       expect(schema.(user: {}).errors.to_h).to eql(user: {or: [{name: ["is missing"]}, {nickname: ["is missing"]}]})
+    end
+  end
+
+  context "with three schemas" do
+    name_schema = Dry::Schema.define do
+      required(:name).filled(:string)
+      required(:surname).filled(:string)
+    end
+
+    nickname_schema = Dry::Schema.define do
+      required(:nickname).filled(:string)
+    end
+
+    email_schema = Dry::Schema.define do
+      required(:email).filled(:string)
+    end
+
+    subject(:schema) do
+      Dry::Schema.define do
+        required(:user) { name_schema | nickname_schema | email_schema }
+      end
+    end
+
+    it "returns success for valid input" do
+      expect(schema.(user: {name: "John", surname: "Smith"})).to be_success
+      expect(schema.(user: {nickname: "John"})).to be_success
+      expect(schema.(user: {email: "test@example.com"})).to be_success
+    end
+
+    it "provides error messages for invalid input where both sides failed" do
+      expect(schema.(user: {}).errors.to_h).to eql(user: {or: [{name: ["is missing"], surname: ["is missing"]}, {nickname: ["is missing"]}, email: ["is missing"]]})
+    end
+  end
+
+  context "with multiple schemas and a single field" do
+    first_schema = Dry::Schema.define do
+      required(:type).filled(Types::String.enum(["1"]))
+    end
+    second_schema = Dry::Schema.define do
+      required(:type).filled(Types::String.enum(["2"]))
+    end
+    third_schema = Dry::Schema.define do
+      required(:type).filled(Types::String.enum(["3"]))
+    end
+    fourth_schema = Dry::Schema.define do
+      required(:type).filled(Types::String.enum(["4"]))
+    end
+    main_schema = Dry::Schema.define do
+      required(:t).hash(first_schema | second_schema | third_schema | fourth_schema)
+    end
+
+    it "returns success for valid input" do
+      expect(main_schema.(t: { type: "1" })).to be_success
+      expect(main_schema.(t: { type: "2" })).to be_success
+      expect(main_schema.(t: { type: "3" })).to be_success
+      expect(main_schema.(t: { type: "4" })).to be_success
+    end
+
+    it "provides error messages for invalid input where both sides failed" do
+      expect(main_schema.(t: { type: "15" }).errors.to_h).to eql(t: {or: [{type: ["must be one of: 1 or must be one of: 2 or must be one of: 3 or must be one of: 4"] }] })
+    end
+  end
+
+  context "with complex multiple schemas with inner nestings" do
+    first_schema = Dry::Schema.define do
+      required(:event).filled(Types::String.enum("1"))
+      required(:name).filled(:string)
+      required(:timestamp).filled(:time)
+    end
+
+    second_schema = Dry::Schema.define do
+      required(:event).filled(Types::String.enum("2"))
+      required(:name).filled(:string)
+    end
+
+    third_schema = Dry::Schema.define do
+      required(:event).filled(Types::String.enum("3"))
+      required(:timestamp).filled(:time)
+    end
+
+    fourth_schema = Dry::Schema.define do
+      required(:event).filled(Types::String.enum("4"))
+      required(:timestamp).filled(:time)
+    end
+
+    main_schema = Dry::Schema.define do
+      required(:events).value(:array).each do
+        hash? & (first_schema | second_schema | third_schema | fourth_schema)
+      end
+    end
+
+    it "returns success for valid input" do
+      expect(main_schema.(events: [])).to be_success
+      expect(main_schema.(events: [{event: "1", name: "Hello", timestamp: "2021-11-30T16:22:58+00:00"}])).to be_success
+      expect(main_schema.(events: [{event: "2", name: "Hello"}])).to be_success
+      expect(main_schema.(events: [{event: "4", name: "Hello", timestamp: Time.now}])).to be_success
+    end
+
+    it "provides error messages for invalid input where both sides failed" do
+      expect(
+        main_schema.(events: [{event: "1", timestamp: Time.now}]).errors.to_h
+      ).to(
+        eql(
+          events: {
+            0 => {
+                or: [
+                   {:event=>["must be one of: 1"], :name=>["is missing"]},
+                   {:event=>["must be one of: 2"]},
+                   {:event=>["must be one of: 3"]},
+                   {:event=>["must be one of: 4"]}
+                ]
+            }
+          }
+        )
+      )
+
+      expect(
+        main_schema.(events: [{event: "4"}]).errors.to_h
+      ).to(
+        eql(
+          events: {
+            0 => {
+              or: [
+                {:event=>["must be one of: 1"], :name=>["is missing"], :timestamp=>["is missing"]},
+                {:event=>["must be one of: 2"], :timestamp=>["is missing"]},
+                {:event=>["must be one of: 3"], :timestamp=>["is missing"]},
+                {:event=>["must be one of: 4"], :timestamp=>["is missing"]}
+              ]
+            }
+          }
+        )
+      )
+      expect(
+        main_schema.(events: [{event: "5"}]).errors.to_h
+      ).to(
+        eql(
+          events: {
+            0 => {
+              or: [
+                {:event=>["must be one of: 1"], :name=>["is missing"], :timestamp=>["is missing"]},
+                {:event=>["must be one of: 2"], :timestamp=>["is missing"]},
+                {:event=>["must be one of: 3"], :timestamp=>["is missing"]},
+                {:event=>["must be one of: 4"], :timestamp=>["is missing"]}
+              ]
+            }
+          }
+        )
+      )
     end
   end
 end
