37c84f27b788846d89e6139b2b606c642fdd70bc
Rob Hanlon <robhanlon@gmail.com>
2022-09-08 15:49:21 -0700

    Incorporate CalendarDate test from #423

--- spec/integration/schema/custom_types_spec.rb
+++ spec/integration/schema/custom_types_spec.rb
@@ -162,6 +162,45 @@ RSpec.describe "Registering custom types" do
           expect(result[:user][:age]).to eql("i am not that old")
         end
       end
+
+      context "custom constructor" do
+        subject(:schema) do
+          Dry::Schema.Params do
+            config.types = ContainerWithTypes
+            optional(:date).maybe(:calendar_day)
+          end
+        end
+
+        let(:calendar_date) do
+          Class.new(::Date) do
+            def self.parse(date)
+              mon, mday = Date._iso8601(date).values_at(:mon, :mday)
+              raise ArgumentError, "invalid ISO8601 calendar day string, expected format \"--MM-DD\"" unless mon && mday
+
+              new(2000, mon, mday)
+            end
+          end
+        end
+
+        before do
+          stub_const("CalendarDate", calendar_date)
+
+          container_with_types.register(
+            :calendar_day,
+            Types::Strict(CalendarDate).constructor(CalendarDate.method(:parse))
+          )
+        end
+
+        let(:params) do
+          {"date" => "--02-09"}
+        end
+
+        specify do
+          expect(result).to be_success
+          expect(result[:date]).to be_a(CalendarDate)
+          expect(result[:date].strftime("--%m-%d")).to eq("--02-09")
+        end
+      end
     end
   end
 
