98e8c827cc909a5e50b100552028ac17bcc0a15b
Nikita Shilnikov <fg@flashgordon.ru>
2023-08-25 13:39:16 +0200

    Infer types from nested structs (fixes #464)
    It required an AST transformation. Other approaches I tried had various flaws.

--- lib/dry/schema/extensions/struct.rb
+++ lib/dry/schema/extensions/struct.rb
@@ -5,19 +5,41 @@ require "dry/struct"
 module Dry
   module Schema
     module Macros
+      class StructToSchema < ::Dry::Struct::Compiler
+        def call(struct)
+          visit(struct.to_ast)
+        end
+
+        # strip away structs from AST
+        def visit_struct(node)
+          _, ast = node
+          visit(ast)
+        end
+      end
+
+      Hash.option :struct_compiler, default: proc { StructToSchema.new(schema_dsl.config.types) }
+
       Hash.prepend(::Module.new {
         def call(*args)
-          if args.size >= 1 && args[0].is_a?(::Class) && args[0] <= ::Dry::Struct
+          if args.size >= 1 && struct?(args[0])
             if block_given?
               raise ArgumentError, "blocks are not supported when using "\
                                    "a struct class (#{name.inspect} => #{args[0]})"
             end
 
-            super(args[0].schema, *args.drop(1))
-            type(schema_dsl.types[name].constructor(args[0].schema))
+            schema = struct_compiler.(args[0])
+
+            super(schema, *args.drop(1))
+            type(schema_dsl.types[name].constructor(schema))
           else
             super
           end
+        end
+
+        private
+
+        def struct?(type)
+          type.is_a?(::Class) && type <= ::Dry::Struct
         end
       })
     end
