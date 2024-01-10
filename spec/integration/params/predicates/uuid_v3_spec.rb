385a43a568e32fa265871b2c1102b5aa11cb36b5
Spyros Kazanas <skazanas@dialecticanet.com>
2024-01-11 00:27:18 +0200

    Add error message for uuid_v1? uuid_v2? uuid_v3? and uuid_v5?

---
+++ spec/integration/params/predicates/uuid_v3_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec.describe "Predicates: uuid_v3?" do
+  subject(:schema) do
+    Dry::Schema.Params do
+      required(:uuid).value(:string, :uuid_v3?)
+    end
+  end
+
+  it "passes with valid input" do
+    expect(schema.(uuid: '1d955abe-9522-33d5-a788-a1b186b163dc')).to be_success
+  end
+
+  it "fails with invalid input" do
+    expect(schema.(uuid: "not-uuid").errors.to_h).to eql(uuid: ["is not a valid UUID"])
+  end
+end
---
+++ spec/integration/params/predicates/uuid_v5_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec.describe "Predicates: uuid_v5?" do
+  subject(:schema) do
+    Dry::Schema.Params do
+      required(:uuid).value(:string, :uuid_v5?)
+    end
+  end
+
+  it "passes with valid input" do
+    expect(schema.(uuid: '601d90d0-8611-502d-b49b-86c0779b6159')).to be_success
+  end
+
+  it "fails with invalid input" do
+    expect(schema.(uuid: "not-uuid").errors.to_h).to eql(uuid: ["is not a valid UUID"])
+  end
+end
