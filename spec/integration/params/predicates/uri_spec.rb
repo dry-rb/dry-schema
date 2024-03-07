441cc0e64ce6d2e2c8c705ed02cfd03b105c32e2
Tim_Jones <tjones@aurorasolar.com>
2024-03-07 13:43:28 -0500

    Rubocop fixes

--- spec/integration/params/predicates/uri_spec.rb
+++ spec/integration/params/predicates/uri_spec.rb
@@ -1,15 +1,17 @@
-RSpec.describe 'Predicates: uri?' do
+# frozen_string_literal: true
+
+RSpec.describe "Predicates: uri?" do
   subject(:schema) do
     Dry::Schema.Params do
       required(:uri) { str? & uri?(:https) }
     end
   end
 
-  it 'passes with valid input' do
+  it "passes with valid input" do
     expect(schema.(uri: "https://www.example.com")).to be_success
   end
 
-  it 'fails with invalid input' do
-    expect(schema.(uri: 'not-a-uri').errors.to_h).to eql(uri: ['is not a valid URI'])
+  it "fails with invalid input" do
+    expect(schema.(uri: "not-a-uri").errors.to_h).to eql(uri: ["is not a valid URI"])
   end
 end
