8cb064f1c975a370ee2362a4be86062162459caa
Spyros Kazanas <skazan@proton.me>
2024-11-21 00:54:09 +0200

    Add error message for uuid_v1? uuid_v2? uuid_v3? and uuid_v5? (#474)

---
+++ spec/integration/params/predicates/uuid_v1_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec.describe "Predicates: uuid_v1?" do
+  subject(:schema) do
+    Dry::Schema.Params do
+      required(:uuid).value(:string, :uuid_v1?)
+    end
+  end
+
+  it "passes with valid input" do
+    expect(schema.(uuid: "2e14d58e-afff-11ee-a506-0242ac120002")).to be_success
+  end
+
+  it "fails with invalid input" do
+    expect(schema.(uuid: "not-uuid").errors.to_h).to eql(uuid: ["is not a valid UUID"])
+  end
+end
