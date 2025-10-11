358afbe2948aec4241d58aba4b80795c61d1a722
Brandon Weaver <keystonelemur@gmail.com>
2025-10-11 13:46:01 -0700

    Fix Rubocop issues: trailing whitespace and empty lines

--- spec/integration/schema/intersection_types_spec.rb
+++ spec/integration/schema/intersection_types_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Intersection types" do
     let(:schema) do
       Dry::Schema.Params do
         required(:body).value(
-          Types::Hash.schema(a: Types::String) & 
+          Types::Hash.schema(a: Types::String) &
           (Types::Hash.schema(b: Types::String) | Types::Hash.schema(c: Types::String))
         )
       end
@@ -13,21 +13,21 @@ RSpec.describe "Intersection types" do
 
     it "validates intersection of hash schemas successfully" do
       result = schema.call(body: {a: "test", b: "value"})
-      
+
       expect(result).to be_success
       expect(result.to_h).to eq(body: {a: "test", b: "value"})
     end
 
     it "validates intersection with alternative branch" do
       result = schema.call(body: {a: "test", c: "value"})
-      
+
       expect(result).to be_success
       expect(result.to_h).to eq(body: {a: "test", c: "value"})
     end
 
     it "fails when intersection requirements not met" do
       result = schema.call(body: {b: "value"})
-      
+
       expect(result).to be_failure
       expect(result.errors.to_h).to eq(body: {a: ["is missing"]})
     end
@@ -42,7 +42,7 @@ RSpec.describe "Intersection types" do
 
     it "validates simple intersection types" do
       result = schema.call(value: "test")
-      
+
       expect(result).to be_success
       expect(result.to_h).to eq(value: "test")
     end
@@ -57,14 +57,14 @@ RSpec.describe "Intersection types" do
 
     it "combines type intersection with predicate rules" do
       result = schema.call(name: "John")
-      
+
       expect(result).to be_success
       expect(result.to_h).to eq(name: "John")
     end
 
     it "fails when predicate rules not met" do
       result = schema.call(name: "J")
-      
+
       expect(result).to be_failure
       expect(result.errors.to_h).to eq(name: ["size cannot be less than 2"])
     end
